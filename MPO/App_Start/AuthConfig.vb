dc4bb3ffdaba1f26881ded06077db65dc8b4138f
francogiacobbi <63289005+francogiacobbi@users.noreply.github.com>
2022-01-12 17:43:55 +0100

    Add files via upload

---
+++ MPO/App_Start/AuthConfig.vb
@@ -0,0 +1,22 @@
+﻿Imports Microsoft.AspNet.Membership.OpenAuth
+
+Public Module AuthConfig
+    Sub RegisterOpenAuth()
+        ' Visitare http://go.microsoft.com/fwlink/?LinkId=252803 per informazioni dettagliate sulla configurazione di questa applicazione ASP.NET
+        ' per il supporto dell'accesso tramite servizi esterni.
+
+        'OpenAuth.AuthenticationClients.AddTwitter(
+        '    consumerKey:= "codice utente Twitter personale",
+        '    consumerSecret:= "segreto utente Twitter personale")
+
+        'OpenAuth.AuthenticationClients.AddFacebook(
+        '    appId:= "ID app Facebook personale",
+        '    appSecret:= "segreto app Facebook personale")
+
+        'OpenAuth.AuthenticationClients.AddMicrosoft(
+        '    clientId:= "ID client account Microsoft personale",
+        '    clientSecret:= "segreto client account Microsoft personale")
+
+        'OpenAuth.AuthenticationClients.AddGoogle()
+    End Sub
+End Module
---
+++ MPO/App_Start/BundleConfig.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.Optimization
+
+Public Class BundleConfig
+    ' Per ulteriori informazioni sulla creazione di bundle, visitare http://go.microsoft.com/fwlink/?LinkId=254726
+    Public Shared Sub RegisterBundles(ByVal bundles As BundleCollection)
+        bundles.Add(New ScriptBundle("~/bundles/WebFormsJs").Include(
+                        "~/Scripts/WebForms/WebForms.js",
+                        "~/Scripts/WebForms/WebUIValidation.js",
+                        "~/Scripts/WebForms/MenuStandards.js",
+                        "~/Scripts/WebForms/Focus.js",
+                        "~/Scripts/WebForms/GridView.js",
+                        "~/Scripts/WebForms/DetailsView.js",
+                        "~/Scripts/WebForms/TreeView.js",
+                        "~/Scripts/WebForms/WebParts.js"))
+
+        ' L'ordine è molto importante per il funzionamento di questi file poiché hanno dipendenze esplicite
+        bundles.Add(New ScriptBundle("~/bundles/MsAjaxJs").Include(
+                "~/Scripts/WebForms/MsAjax/MicrosoftAjax.js",
+                "~/Scripts/WebForms/MsAjax/MicrosoftAjaxApplicationServices.js",
+                "~/Scripts/WebForms/MsAjax/MicrosoftAjaxTimer.js",
+                "~/Scripts/WebForms/MsAjax/MicrosoftAjaxWebForms.js"))
+
+        ' Utilizzare la versione di sviluppo di Modernizr per eseguire attività di sviluppo ed esercizi. Successivamente, quando si è
+        ' pronti per passare alla produzione, utilizzare lo strumento di compilazione disponibile all'indirizzo http://modernizr.com per selezionare solo i test necessari
+        bundles.Add(New ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"))
+       
+    End Sub
+End Class
